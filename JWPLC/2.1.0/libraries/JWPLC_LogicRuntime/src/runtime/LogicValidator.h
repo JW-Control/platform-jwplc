ad38675e32b24bc5c2b2b1e1cb5c1f57b97ce632
JW-Control <jw.control.peru@gmail.com>
2026-07-12 11:16:47 -0500

    feat(logic-runtime): add program validator API

---
+++ JWPLC/2.1.0/libraries/JWPLC_LogicRuntime/src/runtime/LogicValidator.h
@@ -0,0 +1,37 @@
+#ifndef JWPLC_LOGIC_VALIDATOR_H
+#define JWPLC_LOGIC_VALIDATOR_H
+
+#include <Arduino.h>
+
+#include "LogicProgram.h"
+
+enum class LogicValidationError : uint8_t
+{
+  None = 0,
+  NullProgram,
+  EmptyProgram,
+  TooManyBlocks,
+  InvalidBlockType,
+  MissingSource,
+  SourceOutOfRange,
+  SourceNotPrevious,
+  ResourceOutOfRange,
+  DuplicateDigitalOutput
+};
+
+class LogicValidator
+{
+public:
+  static LogicValidationError validate(const LogicProgram &program,
+                                       uint16_t maxBlocks,
+                                       uint8_t digitalInputCount,
+                                       uint8_t digitalOutputCount);
+
+  static const char *errorName(LogicValidationError error);
+
+private:
+  static LogicValidationError validateSource(uint16_t source,
+                                             uint16_t currentBlockIndex);
+};
+
+#endif
