ac14b3e00e4e4af970eaa6729f97a7d5109205c7
JW-Control <jw.control.peru@gmail.com>
2026-07-14 20:38:26 -0500

    feat(logic-runtime-ui): definir pantalla principal USER

---
+++ JWPLC/2.1.0/libraries/JWPLC_LogicRuntime_UI/src/screens/RuntimeUIHome.h
@@ -0,0 +1,42 @@
+#ifndef JWPLC_LOGIC_RUNTIME_UI_HOME_H
+#define JWPLC_LOGIC_RUNTIME_UI_HOME_H
+
+#include <Arduino.h>
+#include <JWPLC_LogicRuntime.h>
+#include <JWPLC_Display.h>
+#include <JWPLC_GlobalPeripherals.h>
+
+class RuntimeUIHome
+{
+public:
+  RuntimeUIHome();
+
+  void attach(JWPLC_LogicRuntime &runtime);
+  void enter();
+  void refresh(const JWPLC_IOState *io,
+               const JWPLC_RTCState *rtc);
+  void exit();
+  void forceRedraw();
+
+private:
+  static constexpr uint8_t MENU_COUNT = 4;
+
+  void drawStaticLayout();
+  void drawDynamicState();
+  void drawMenu();
+  void handleInput();
+  void showSelectionMessage();
+
+  const char *programName() const;
+  uint16_t runtimeStateColor() const;
+  const char *runtimeStateText() const;
+
+  JWPLC_LogicRuntime *_runtime;
+  bool _fullRedraw;
+  uint8_t _selectedMenu;
+  uint8_t _lastSelectedMenu;
+  uint32_t _lastDynamicRefreshMs;
+  uint32_t _messageUntilMs;
+};
+
+#endif
