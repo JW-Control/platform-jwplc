7042a95a27f059438b6ac98e5d9dcc803b89ebf2
JW-Control <jw.control.peru@gmail.com>
2026-07-15 23:16:19 -0500

    feat(logic-runtime-ui): activar renderer FBD v0.4.4

--- JWPLC/2.1.0/libraries/JWPLC_LogicRuntime_UI/src/JWPLC_LogicRuntime_UI.h
+++ JWPLC/2.1.0/libraries/JWPLC_LogicRuntime_UI/src/JWPLC_LogicRuntime_UI.h
@@ -11,7 +11,7 @@
 #include "screens/RuntimeUIProgram.h"
 #include "screens/RuntimeUIDiagram.h"
 #include "screens/RuntimeUIBlocks.h"
-#include "screens/RuntimeUIFBDMapV2.h"
+#include "screens/RuntimeUIFBDMapV3.h"
 
 /**
  * @brief Interfaz USER modular del JWPLC Logic Runtime.
@@ -92,7 +92,7 @@ private:
   RuntimeUIDiagram _diagram;
   RuntimeUIBlocks _blocks;
   RuntimeUIV2ReadModel _v2Model;
-  RuntimeUIFBDMapV2 _fbdMapV2;
+  RuntimeUIFBDMapV3 _fbdMapV2;
 };
 
 extern JWPLC_LogicRuntime_UIClass JWPLC_LogicRuntime_UI;
