f84c7b070ee9e16a765b9e4dbd0a18dde1e627b6
JW-Control <jw.control.peru@gmail.com>
2026-07-16 20:50:12 -0500

    chore(ui): actualizar ejemplo FBD a v0.4.7

--- JWPLC/2.1.0/libraries/JWPLC_LogicRuntime_UI/examples/JWPLC_LogicRuntime_UI_FBD_Map_V2_RAM/JWPLC_LogicRuntime_UI_FBD_Map_V2_RAM.ino
+++ JWPLC/2.1.0/libraries/JWPLC_LogicRuntime_UI/examples/JWPLC_LogicRuntime_UI_FBD_Map_V2_RAM/JWPLC_LogicRuntime_UI_FBD_Map_V2_RAM.ino
@@ -153,7 +153,7 @@ void setup()
   JWPLC_LogicRuntime_UI.begin(engine);
 
   Serial.println("Motor v2: RUNNING");
-  Serial.println("UI FBD v0.4.6: LISTA");
+  Serial.println("UI FBD v0.4.7: LISTA");
   Serial.println("Pulse cualquier boton para entrar a USER.");
 }
 
