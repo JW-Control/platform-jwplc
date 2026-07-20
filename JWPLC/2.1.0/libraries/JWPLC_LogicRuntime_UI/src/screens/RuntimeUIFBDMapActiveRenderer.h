82e9702efd72d5af1d5a06fa82d70606a18055f4
JW-Control <jw.control.peru@gmail.com>
2026-07-19 20:22:49 -0500

    refactor(ui): cache active TON footer state

--- JWPLC/2.1.0/libraries/JWPLC_LogicRuntime_UI/src/screens/RuntimeUIFBDMapActiveRenderer.h
+++ JWPLC/2.1.0/libraries/JWPLC_LogicRuntime_UI/src/screens/RuntimeUIFBDMapActiveRenderer.h
@@ -89,6 +89,7 @@ private:
   bool _activeElapsedCacheValid;
   bool _activeElapsedColorOn;
   char _activeElapsedCache[20];
+  bool _activeFooterDefaultVisible;
 
   bool _unifiedHeaderCacheValid;
   HeaderView _unifiedHeaderViewCache;
