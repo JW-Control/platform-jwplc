d423f932cfe571864bdfec2f75ff6bdacd72167f
JW-Control <jw.control.peru@gmail.com>
2026-08-09 16:53:07 -0500

    chore(build): limpiar marcadores descartados de P4

--- JWPLC/2.1.0/libraries/JWPLC_Ethernet/src/JWPLC_Autoload_JWPLC_Ethernet.h
+++
@@ -1,5 +0,0 @@
-#ifndef JWPLC_AUTOLOAD_JWPLC_ETHERNET_H
-#define JWPLC_AUTOLOAD_JWPLC_ETHERNET_H
-// Empty marker for Arduino library discovery. The backend Ethernet dependency
-// is discovered later from JWPLC_Ethernet sources; do not force Ethernet.h/ETH.h here.
-#endif
