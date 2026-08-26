3b3d8166e3cab7a822d3da7eb39d80f67097512e
JW-Control <jw.control.peru@gmail.com>
2026-08-26 12:43:33 -0500

    feat(ethernet): implementar DHCP start-poll no bloqueante

--- JWPLC/2.1.0/libraries/JWPLC_Ethernet_W5x00_Backend/src/Dhcp.cpp
+++ JWPLC/2.1.0/libraries/JWPLC_Ethernet_W5x00_Backend/src/Dhcp.cpp
@@ -6,8 +6,29 @@
 #include "Dhcp.h"
 #include "utility/w5100.h"
 
+DhcpClass::DhcpClass()
+	: _dhcpInitialTransactionId(0),
+	  _dhcpTransactionId(0),
+	  _dhcpLeaseTime(0),
+	  _dhcpT1(0),
+	  _dhcpT2(0),
+	  _renewInSec(0),
+	  _rebindInSec(0),
+	  _timeout(0),
+	  _responseTimeout(0),
+	  _lastCheckLeaseMillis(0),
+	  _dhcp_state(STATE_DHCP_START),
+	  _asyncActive(false),
+	  _asyncStartTime(0),
+	  _asyncStateStartTime(0)
+{
+	memset(_dhcpMacAddr, 0, sizeof(_dhcpMacAddr));
+	reset_DHCP_lease();
+}
+
 int DhcpClass::beginWithDHCP(uint8_t *mac, unsigned long timeout, unsigned long responseTimeout)
 {
+	_asyncActive = false;
 	_dhcpLeaseTime=0;
 	_dhcpT1=0;
 	_dhcpT2=0;
@@ -23,13 +44,157 @@ int DhcpClass::beginWithDHCP(uint8_t *mac, unsigned long timeout, unsigned long 
 	return request_DHCP_lease();
 }
 
+int DhcpClass::beginWithDHCPAsync(uint8_t *mac, unsigned long timeout, unsigned long responseTimeout)
+{
+	_asyncActive = false;
+	_dhcpLeaseTime = 0;
+	_dhcpT1 = 0;
+	_dhcpT2 = 0;
+	_timeout = timeout;
+	_responseTimeout = responseTimeout;
+
+	memset(_dhcpMacAddr, 0, 6);
+	reset_DHCP_lease();
+	memcpy((void*)_dhcpMacAddr, (void*)mac, 6);
+
+	_dhcpTransactionId = random(1UL, 2000UL);
+	_dhcpInitialTransactionId = _dhcpTransactionId;
+	_dhcp_state = STATE_DHCP_START;
+
+	_dhcpUdpSocket.stop();
+	if (_dhcpUdpSocket.begin(DHCP_CLIENT_PORT) == 0) {
+		return -1;
+	}
+
+	presend_DHCP();
+
+	_asyncStartTime = millis();
+	_asyncStateStartTime = _asyncStartTime;
+	_asyncActive = true;
+
+	// Envía el primer DISCOVER en esta misma llamada, sin esperar respuesta.
+	return pollDHCP();
+}
+
+int DhcpClass::pollDHCP()
+{
+	if (!_asyncActive) {
+		return (_dhcp_state == STATE_DHCP_LEASED) ? 1 : -1;
+	}
+
+	unsigned long now = millis();
+
+	if ((now - _asyncStartTime) > _timeout) {
+		finishAsync();
+		return -1;
+	}
+
+	uint8_t messageType = 0;
+	uint32_t respId = 0;
+
+	if (_dhcp_state == STATE_DHCP_START) {
+		_dhcpTransactionId++;
+		send_DHCP_MESSAGE(DHCP_DISCOVER, ((now - _asyncStartTime) / 1000));
+		_dhcp_state = STATE_DHCP_DISCOVER;
+		_asyncStateStartTime = now;
+		return 0;
+	}
+
+	if (_dhcp_state == STATE_DHCP_REREQUEST) {
+		_dhcpTransactionId++;
+		send_DHCP_MESSAGE(DHCP_REQUEST, ((now - _asyncStartTime) / 1000));
+		_dhcp_state = STATE_DHCP_REQUEST;
+		_asyncStateStartTime = now;
+		return 0;
+	}
+
+	if (_dhcp_state == STATE_DHCP_DISCOVER) {
+		messageType = parseDHCPResponseAvailable(respId);
+
+		if (messageType == DHCP_OFFER) {
+			_dhcpTransactionId = respId;
+			send_DHCP_MESSAGE(DHCP_REQUEST, ((now - _asyncStartTime) / 1000));
+			_dhcp_state = STATE_DHCP_REQUEST;
+			_asyncStateStartTime = now;
+			return 0;
+		}
+
+		if ((now - _asyncStateStartTime) > _responseTimeout) {
+			_dhcp_state = STATE_DHCP_START;
+			_asyncStateStartTime = now;
+		}
+
+		return 0;
+	}
+
+	if (_dhcp_state == STATE_DHCP_REQUEST) {
+		messageType = parseDHCPResponseAvailable(respId);
+
+		if (messageType == DHCP_ACK) {
+			_dhcp_state = STATE_DHCP_LEASED;
+			finalizeLease();
+			finishAsync();
+			return 1;
+		}
+
+		if (messageType == DHCP_NAK) {
+			_dhcp_state = STATE_DHCP_START;
+			_asyncStateStartTime = now;
+			return 0;
+		}
+
+		if ((now - _asyncStateStartTime) > _responseTimeout) {
+			_dhcp_state = STATE_DHCP_START;
+			_asyncStateStartTime = now;
+		}
+
+		return 0;
+	}
+
+	if (_dhcp_state == STATE_DHCP_LEASED) {
+		finishAsync();
+		return 1;
+	}
+
+	_dhcp_state = STATE_DHCP_START;
+	_asyncStateStartTime = now;
+	return 0;
+}
+
 void DhcpClass::reset_DHCP_lease()
 {
 	// zero out _dhcpSubnetMask, _dhcpGatewayIp, _dhcpLocalIp, _dhcpDhcpServerIp, _dhcpDnsServerIp
 	memset(_dhcpLocalIp, 0, 20);
 }
 
-	//return:0 on error, 1 if request is sent and response is received
+void DhcpClass::finalizeLease()
+{
+	// use default lease time if we didn't get it
+	if (_dhcpLeaseTime == 0) {
+		_dhcpLeaseTime = DEFAULT_LEASE;
+	}
+
+	// Calculate T1 & T2 if we didn't get it
+	if (_dhcpT1 == 0) {
+		_dhcpT1 = _dhcpLeaseTime >> 1;
+	}
+	if (_dhcpT2 == 0) {
+		_dhcpT2 = _dhcpLeaseTime - (_dhcpLeaseTime >> 3);
+	}
+
+	_renewInSec = _dhcpT1;
+	_rebindInSec = _dhcpT2;
+}
+
+void DhcpClass::finishAsync()
+{
+	_dhcpUdpSocket.stop();
+	_dhcpTransactionId++;
+	_lastCheckLeaseMillis = millis();
+	_asyncActive = false;
+}
+
+// return:0 on error, 1 if request is sent and response is received
 int DhcpClass::request_DHCP_lease()
 {
 	uint8_t messageType = 0;
@@ -75,21 +240,7 @@ int DhcpClass::request_DHCP_lease()
 			if (messageType == DHCP_ACK) {
 				_dhcp_state = STATE_DHCP_LEASED;
 				result = 1;
-				//use default lease time if we didn't get it
-				if (_dhcpLeaseTime == 0) {
-					_dhcpLeaseTime = DEFAULT_LEASE;
-				}
-				// Calculate T1 & T2 if we didn't get it
-				if (_dhcpT1 == 0) {
-					// T1 should be 50% of _dhcpLeaseTime
-					_dhcpT1 = _dhcpLeaseTime >> 1;
-				}
-				if (_dhcpT2 == 0) {
-					// T2 should be 87.5% (7/8ths) of _dhcpLeaseTime
-					_dhcpT2 = _dhcpLeaseTime - (_dhcpLeaseTime >> 3);
-				}
-				_renewInSec = _dhcpT1;
-				_rebindInSec = _dhcpT2;
+				finalizeLease();
 			} else if (messageType == DHCP_NAK) {
 				_dhcp_state = STATE_DHCP_START;
 			}
@@ -235,9 +386,6 @@ void DhcpClass::send_DHCP_MESSAGE(uint8_t messageType, uint16_t secondsElapsed)
 
 uint8_t DhcpClass::parseDHCPResponse(unsigned long responseTimeout, uint32_t& transactionId)
 {
-	uint8_t type = 0;
-	uint8_t opt_len = 0;
-
 	unsigned long startTime = millis();
 
 	while (_dhcpUdpSocket.parsePacket() <= 0) {
@@ -246,6 +394,24 @@ uint8_t DhcpClass::parseDHCPResponse(unsigned long responseTimeout, uint32_t& tr
 		}
 		delay(50);
 	}
+
+	return parseDHCPResponsePacket(transactionId);
+}
+
+uint8_t DhcpClass::parseDHCPResponseAvailable(uint32_t& transactionId)
+{
+	if (_dhcpUdpSocket.parsePacket() <= 0) {
+		return 0;
+	}
+
+	return parseDHCPResponsePacket(transactionId);
+}
+
+uint8_t DhcpClass::parseDHCPResponsePacket(uint32_t& transactionId)
+{
+	uint8_t type = 0;
+	uint8_t opt_len = 0;
+
 	// start reading in the packet
 	RIP_MSG_FIXED fixedMsg;
 	_dhcpUdpSocket.read((uint8_t*)&fixedMsg, sizeof(RIP_MSG_FIXED));
