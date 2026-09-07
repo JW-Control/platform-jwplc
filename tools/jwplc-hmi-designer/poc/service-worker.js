2806104e3cb6da5ef8087002c50e68f920869182
JW-Control <jw.control.peru@gmail.com>
2026-09-07 12:38:27 -0500

    chore(alpha11): cachear estabilidad Pixel A11-7B

--- tools/jwplc-hmi-designer/poc/service-worker.js
+++ tools/jwplc-hmi-designer/poc/service-worker.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CACHE_NAME = 'jwplc-hmi-designer-alpha11-a11-7b-pixel-layer-editing';
+const CACHE_NAME = 'jwplc-hmi-designer-alpha11-a11-7b-pixel-stability';
 const CORE = [
   './desktop.html',
   './index.html',
@@ -16,6 +16,7 @@ const CORE = [
   './designer-field-visibility.js',
   './designer-pixelmap-compat.js',
   './designer-pixelmap.js',
+  './designer-pixelmap-stability.js',
   './designer-inspector-state.js',
   './designer-pages.js',
   './designer-live.js',
